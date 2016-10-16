6f2275241e92946072922758cd2676c46845182d
CF12 <brianx0011@gmail.com>
2016-10-16 13:51:46 -0400

    Added docker support and added purging

--- src/bot.js
+++ src/bot.js
@@ -3,17 +3,44 @@ const Eris = require('eris')
 const fs = require('fs')
 const path = require('path')
 
-// Setting up objects
+// Setting up objects & vars
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')))
 let bot = new Eris(config.bot_token)
+let pf = '$'
 
 // Attempts to connect the bot
-try {
-  bot.connect()
-} catch (err) {
-  if (err) throw err
-  console.log('ERROR > Error while connecting to discord. Check your bot token.')
+bot.connect()
+
+// Class: Logger
+function Logger () {
+  // Log to console
+  Logger.prototype.logConsole = (log, type) => {
+    if (type === 'info') {
+      console.log('INFO > ' + log)
+    } else if (type === 'err') {
+      console.log('ERROR > ' + log)
+    } else if (type === 'debug') {
+      console.log('DEBUG > ' + log)
+    } else {
+      console.log('ERROR > Invalid console log')
+    }
+  }
+
+  // Log to channel
+  Logger.prototype.logChannel = (log, type) => {
+    if (type === 'info') {
+      msg.channel.createMessage('```> INFO: ' + log + '```')
+    } else if (type === 'err') {
+      msg.channel.createMessage('```> ERROR: ' + log + '```')
+    } else {
+      console.log('ERROR > Invalid channel log')
+    }
+  }
 }
+
+// Vars and Object Init
+let msg = null
+let logger = new Logger()
 
 // On: Bot Ready
 bot.on('ready', () => {
@@ -21,7 +48,43 @@ bot.on('ready', () => {
 })
 
 bot.on('messageCreate', (msg) => {
-  if (msg.content.startsWith('ping')) {
+  // Sets global msg var to local msg var
+  msg = msg
+
+  // Simple Ping Command
+  if (msg.content.startsWith(pf + 'ping')) {
     msg.channel.createMessage('Pong!')
+  }
+
+  // Purge Command
+  if (msg.content.startsWith(pf + 'purge')) {
+    let arg = msg.content.split(' ')
+    arg = arg[1]
+    console.log('DEBUG > var arg === ' + arg)
+
+    if (arg % 1 === 0 && arg >= 1 && arg <= 100) {
+      if (msg.member.permission.has('manageMessages')) {
+        msg.channel.purge(arg)
+        logger.logChannel('Successfully purged ' + arg + ' messages.', 'info')
+      } else {
+        logger.logChannel('User does not have the manageMessages permission!', 'err')
+        return
+      }
+    } else {
+      logger.logChannel('Invalid arguments! Use an integer from 1 to 100 for your parameter.', 'err')
+    }
+  }
+
+  // Debug Command
+  if (msg.content.startsWith(pf + 'db')) {
+    logger.logConsole(msg.member.permission.has('manageMessages'), 'debug')
+  }
+})
+
+// On: Bot Error
+bot.on('error', (err, id) => {
+  if (err) {
+    logger.logConsole('Unknown error has occured to shard ' + id + '. Please see stack trace', 'err')
+    throw err
   }
 })
